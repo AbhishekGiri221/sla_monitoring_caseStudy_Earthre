2ee55f4d2bb5ffc7aea44b42fab7044badec1317
Abhishek Giri <abhishekgiri@Abhisheks-MacBook-Air.local>
2026-09-17 01:35:58 +0530

    Added statusCode_validator to validate and clean status code

---
+++ statusCode_validator.js
@@ -0,0 +1,17 @@
+const cleanStatusCode = (statusCode)=>{
+    if(statusCode === null || statusCode === undefined || statusCode === ""){
+        return null
+    }
+
+    const value = Number(statusCode);
+
+    if(Number.isNaN(value)){
+        return null
+    }
+
+    if(statusCode < 100 || statusCode > 599){
+        return null
+    }
+
+    return value;
+}
